84ec3c3f270365c8b5809f030618750ae84c7a8e
AgamezRaul <39956182+AgamezRaul@users.noreply.github.com>
2021-03-26 11:46:00 -0500

    Padre

--- SiColegioMiHogar/ClientApp/src/app/responsable/form-responsable-padre/form-responsable-padre.component.ts
+++ SiColegioMiHogar/ClientApp/src/app/responsable/form-responsable-padre/form-responsable-padre.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-responsable-padre',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormResponsablePadreComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router,
+    private activatedRoute: ActivatedRoute) { }
 
+  formGroup = this.fb.group({
+    ideResponsablePadre: ['', [Validators.required]],
+    nomResponsablePadre: ['', [Validators.required]],
+    fecNacimientoPadre: ['', [Validators.required]],
+    lugNacimientoPadre: ['', [Validators.required]],
+    lugExpedicionPadre: ['', [Validators.required]],
+    tipDocumentoPadre: ['', [Validators.required]],
+    celResponsablePadre: ['', [Validators.required]],
+    profResponsablePadre: ['', [Validators.required]],
+    ocuResponsablePadre: ['', [Validators.required]],
+    entResonsablePadre: ['', [Validators.required]],
+    celEmpresaPadre: ['', [Validators.required]],
+    tipoResponsablePadre: ['', [Validators.required]],
+    correoPadre: ['', [Validators.required]],
+    acudientePadre: ['', [Validators.required]]
+  });
   ngOnInit() {
   }
 
+  get ideResponsablePadre() {
+    return this.formGroup.get('ideResponsablePadre');
+  }
+  get nomResponsablePadre() {
+    return this.formGroup.get('nomResponsablePadre');
+  }
+  get fecNacimientoPadre() {
+    return this.formGroup.get('fecNacimientoPadre');
+  }
+  get lugNacimientoPadre() {
+    return this.formGroup.get('lugNacimientoPadre');
+  }
+  get lugExpedicionPadre() {
+    return this.formGroup.get('lugExpedicionPadre');
+  }
+  get tipDocumentoPadre() {
+    return this.formGroup.get('tipDocumentoPadre');
+  }
+  get celResponsablePadre() {
+    return this.formGroup.get('celResponsablePadre');
+  }
+  get profResponsablePadre() {
+    return this.formGroup.get('profResponsablePadre');
+  }
+  get ocuResponsablePadre() {
+    return this.formGroup.get('ocuResponsablePadre');
+  }
+  get entResonsablePadre() {
+    return this.formGroup.get('entResonsablePadre');
+  }
+  get celEmpresaPadre() {
+    return this.formGroup.get('celEmpresaPadre');
+  }
+  get tipoResponsablePadre() {
+    return this.formGroup.get('tipoResponsablePadre');
+  }
+  get correoPadre() {
+    return this.formGroup.get('correoPadre');
+  }
+  get acudientePadre() {
+    return this.formGroup.get('acudientePadre');
+  }
 }
--- SiColegioMiHogar/ClientApp/src/app/responsable/responsable.component.ts
+++ SiColegioMiHogar/ClientApp/src/app/responsable/responsable.component.ts
@@ -13,3 +13,52 @@ export class ResponsableComponent implements OnInit {
   }
 
 }
+
+export interface IPadre {
+  ideResponsablePadre: string,
+  nomResponsablePadre: string,
+  fecNacimientoPadre: string,
+  lugNacimientoPadre: string,
+  lugExpedicionPadre: string,
+  tipDocumentoPadre: string,
+  celResponsablePadre: number,
+  profResponsablePadre: string,
+  ocuResponsablePadre: string,
+  entResonsablePadre: string,
+  celEmpresaPadre: number,
+  tipoResponsablePadre: string,
+  correoPadre: string,
+  acudientePadre: string
+}
+export interface IMadre {
+  ideResponsableMadre: string,
+  nomResponsableMadre: string,
+  fecNacimientoMadre: string,
+  lugNacimientoMadre: string,
+  lugExpedicionMadre: string,
+  tipDocumentoMadre: string,
+  celResponsableMadre: number,
+  profResponsableMadre: string,
+  ocuResponsableMadre: string,
+  entResonsableMadre: string,
+  celEmpresaMadre: number,
+  tipoResponsableMadre: string,
+  correoMadre: string,
+  acudienteMadre: string
+}
+export interface IAcudiente {
+  ideResponsableAcudiente: string,
+  nomResponsableAcudiente: string,
+  fecNacimientoAcudiente: string,
+  lugNacimientoAcudiente: string,
+  lugExpedicionAcudiente: string,
+  tipDocumentoAcudiente: string,
+  celResponsableAcudiente: number,
+  profResponsableAcudiente: string,
+  ocuResponsableAcudiente: string,
+  entResonsableAcudiente: string,
+  celEmpresaAcudiente: number,
+  tipoResponsableAcudiente: string,
+  correoAcudiente: string,
+  acudienteAcudiente: string
+}
