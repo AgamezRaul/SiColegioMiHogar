dfa397c2cf8f9382eeee62b1158efd356ab4ca60
Carlos A Daza Murgas <2carlosdaza@gmail.com>
2021-03-26 12:43:01 -0500

    Formulario Madre y Acudiente

--- SiColegioMiHogar/ClientApp/src/app/responsable/form-responsable-acudiente/form-responsable-acudiente.component.ts
+++ SiColegioMiHogar/ClientApp/src/app/responsable/form-responsable-acudiente/form-responsable-acudiente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-responsable-acudiente',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormResponsableAcudienteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router,
+    private activatedRoute: ActivatedRoute) { }
+
+  formGroup = this.fb.group({
+    ideResponsableAcudiente: ['', [Validators.required]],
+    nomResponsableAcudiente: ['', [Validators.required]],
+    fechaNacimientoAcudiente: ['', [Validators.required]],
+    lugNacimientoAcudiente: ['', [Validators.required]],
+    lugExpedicionAcudiente: ['', [Validators.required]],
+    tipoDocumentoAcudiente: ['', [Validators.required]],
+    celResponsableAcudiente: ['', [Validators.required]],
+    profResponsableAcudiente: ['', [Validators.required]],
+    ocuResponsableAcudiente: ['', [Validators.required]],
+    entResponsableAcudiente: ['', [Validators.required]],
+    celEmpresaAcudiente: ['', [Validators.required]],
+    tipoResponsableAcudiente: ['', [Validators.required]],
+    correoAcudiente: ['', [Validators.required]]
+  })
 
   ngOnInit() {
   }
+  get ideResponsableAcudiente() {
+    return this.formGroup.get('ideResponsableAcudiente');
+  }
+  get nomResponsableAcudiente() {
+    return this.formGroup.get('nomResponsableAcudiente');
+  }
+  get fecNacimientoAcudiente() {
+    return this.formGroup.get('fecNacimientoAcudiente');
+  }
+  get lugNacimientoAcudiente() {
+    return this.formGroup.get('lugNacimientoAcudiente');
+  }
+  get tipDocumentoAcudiente() {
+    return this.formGroup.get('tipDocumentoAcudiente');
+  }
+  get celResponsableAcudiente() {
+    return this.formGroup.get('celResponsableAcudiente');
+  }
+  get profResponsableAcudiente() {
+    return this.formGroup.get('profResponsableAcudiente');
+  }
+  get ocuResponsableAcudiente() {
+    return this.formGroup.get('ocuResponsableAcudiente');
+  }
+  get entResponsableAcudiente() {
+    return this.formGroup.get('entResponsableAcudiente');
+  }
+  get celEmpresaAcudiente() {
+    return this.formGroup.get('celEmpresaAcudiente');
+  }
+  get tipoResponsableAcudiente() {
+    return this.formGroup.get('tipoResponsableAcudiente');
+  }
+  get correoAcudiente() {
+    return this.formGroup.get('correoAcudiente');
+  }
 
 }
+
--- SiColegioMiHogar/ClientApp/src/app/responsable/form-responsable-madre/form-responsable-madre.component.ts
+++ SiColegioMiHogar/ClientApp/src/app/responsable/form-responsable-madre/form-responsable-madre.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-responsable-madre',
@@ -7,9 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormResponsableMadreComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router,
+    private activatedRoute: ActivatedRoute) { }
+
+  formGroup = this.fb.group({
+    ideResponsableMadre: ['', [Validators.required]],
+    nomResponsableMadre: ['', [Validators.required]],
+    fechaNacimientoMadre: ['', [Validators.required]],
+    lugNacimientoMadre: ['', [Validators.required]],
+    lugExpedicionMadre: ['', [Validators.required]],
+    tipoDocumentoMadre: ['', [Validators.required]],
+    celResponsableMadre: ['', [Validators.required]],
+    profResponsableMadre: ['', [Validators.required]],
+    ocuResponsableMadre: ['', [Validators.required]],
+    entResponsableMadre: ['', [Validators.required]],
+    celEmpresaMadre: ['', [Validators.required]],
+    tipoResponsableMadre: ['', [Validators.required]],
+    correoMadre: ['', [Validators.required]],
+    acudienteMadre: ['', [Validators.required]]
+  })
 
   ngOnInit() {
   }
 
+  get ideResponsableMadre() {
+    return this.formGroup.get('ideResponsableMadre');
+  }
+  get nomResponsableMadre() {
+    return this.formGroup.get('nomResponsableMadre');
+  }
+  get fecNacimientoMadre() {
+    return this.formGroup.get('fecNacimientoMadre');
+  }
+  get lugNacimientoMadre() {
+    return this.formGroup.get('lugNacimientoMadre');
+  }
+  get tipDocumentoMadre() {
+    return this.formGroup.get('tipDocumentoMadre');
+  }
+  get celResponsableMadre() {
+    return this.formGroup.get('celResponsableMadre');
+  }
+  get profResponsableMadre() {
+    return this.formGroup.get('profResponsableMadre');
+  }
+  get ocuResponsableMadre() {
+    return this.formGroup.get('ocuResponsableMadre');
+  }
+  get entResponsableMadre() {
+    return this.formGroup.get('entResponsableMadre');
+  }
+  get celEmpresaMadre() {
+    return this.formGroup.get('celEmpresaMadre');
+  }
+  get tipoResponsableMadre() {
+    return this.formGroup.get('tipoResponsableMadre');
+  }
+  get correoMadre() {
+    return this.formGroup.get('correoMadre');
+  }
+  get acudienteMadre() {
+    return this.formGroup.get('acudienteMadre');
+  }
 }
