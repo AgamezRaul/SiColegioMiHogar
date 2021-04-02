4148d051d6a437e84ea40c24ff8bcc8597be5847
micheelRojas <56304710+micheelRojas@users.noreply.github.com>
2021-04-01 21:05:28 -0500

    backend de mesnsualidad registar y modificar

---
+++ BackEnd/Mensualidad/Dominio/Repositories/IMensualidadServiceRepository.cs
@@ -0,0 +1,11 @@
+﻿using BackEnd.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd.Mensualidad.Dominio.Repositories
+{
+   public interface IMensualidadServiceRepository : IGenericRepository<Mensualidad>
+    {
+    }
+}
