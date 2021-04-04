f13187f6391b04cc5154bb97efcb3dbf80898eb4
Ismael <ismacenteno123@gmail.com>
2021-04-03 20:40:25 -0500

    Gestión de cursos - Backend

--- BackEnd/MiHagarContextTest.cs
+++ BackEnd/MiHagarContextTest.cs
@@ -22,7 +22,5 @@ namespace BackEnd
         public DbSet<RelacionUR.Dominio.RelacionUR> RelacionUR { get; set; }
         public DbSet<Responsable.Dominio.Responsable> Responsable { get; set; }
         public DbSet<Usuario.Dominio.Usuario> Usuario { get; set; }
-        public DbSet<Curso.Dominio.Curso> Curso { get; set; }
-        public DbSet<Mensualidad.Dominio.Mensualidad> Mensualidad { get; set; }
     }
 }
