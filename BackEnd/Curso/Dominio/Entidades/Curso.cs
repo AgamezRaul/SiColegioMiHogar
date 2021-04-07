47702b882333a03a584ba6402865b7dcd3cac37b
AgamezRaul <39956182+AgamezRaul@users.noreply.github.com>
2021-04-06 22:33:08 -0500

    Corrección de Curso y migración

--- BackEnd/Curso/Dominio/Entidades/Curso.cs
+++ BackEnd/Curso/Dominio/Entidades/Curso.cs
@@ -7,14 +7,11 @@ namespace BackEnd.Curso.Dominio
 {
     public class Curso : Entity<int>
     {
-
-        public int Ide { get; set; }
         public string Nombre { get; set; }
         public int MaxEstudiantes { get; set; }
         public int IdDirectorDocente { get; set; }
-        public Curso(int id, string nombre, int maxEstudiantes, int idDirectorDocente)
+        public Curso(string nombre, int maxEstudiantes, int idDirectorDocente)
         {
-            Ide = id;
             Nombre = nombre;
             MaxEstudiantes = maxEstudiantes;
             IdDirectorDocente = idDirectorDocente;
@@ -25,8 +22,6 @@ namespace BackEnd.Curso.Dominio
 
             if (string.IsNullOrEmpty(curso.Nombre))
                 errors.Add("El campo nombre está vacío");
-            if (curso.Ide <= 0)
-                errors.Add("El id debe ser mayor que cero");
             if (curso.MaxEstudiantes <= 0)
                 errors.Add("El campo debe ser mayor que cero");
             if (curso.IdDirectorDocente <= 0)
