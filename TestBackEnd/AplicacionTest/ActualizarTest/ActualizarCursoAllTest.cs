f3077df702c19f641ff0842d3ff7ba30cce924ac
Ismael <ismacenteno123@gmail.com>
2021-04-15 00:28:46 -0500

    Test Actualizar curso

---
+++ TestBackEnd/AplicacionTest/ActualizarTest/ActualizarCursoAllTest.cs
@@ -0,0 +1,49 @@
+﻿using BackEnd;
+using BackEnd.Base;
+using BackEnd.Curso.Aplicacion.Request;
+using BackEnd.Curso.Aplicacion.Service.Actualizar;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestBackEnd.AplicacionTest.ActualizarTest
+{
+    [TestFixture]
+    public class ActualizarCursoAllTest
+    {
+        MiHogarContextTest _context;
+        UnitOfWork _unitOfWork;
+        ActualizarCursoService _CursoService;
+
+        [SetUp]
+        public void Setup()
+        {
+            var optionsInMemory = new DbContextOptionsBuilder<MiHogarContextTest>().UseInMemoryDatabase("MiHogar").Options;
+            _context = new MiHogarContextTest(optionsInMemory);
+            _unitOfWork = new UnitOfWork(_context);
+        }
+        [TestCaseSource("EditionsCurso")]
+        public void EditarCurso(ActualizarCursoRequest request, string expected)
+        {
+            _CursoService = new ActualizarCursoService(_unitOfWork);
+            var response = _CursoService.Ejecutar(request);
+            Assert.AreEqual(expected, response.Message);
+        }
+        private static IEnumerable<TestCaseData> EditionsCurso()
+        {
+            yield return new TestCaseData(
+                new ActualizarCursoRequest
+                {
+                    id = 1,
+                    nombre = "Primero A",
+                    maxEstudiantes = 20,
+                    idDirectorDocente = 8
+                },
+                "Curso Actualizada Exitosamente"
+                ).SetName("Curso Actualizada Exitosamente");
+        }
+
+    }
+    }
