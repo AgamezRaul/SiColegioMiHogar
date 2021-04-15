8ed813841701067cf976a1422348949122d1167c
jhonRueda <jjrueda@unicesar.edu.co>
2021-04-15 08:44:34 -0500

    agragando servicio consultar materias

---
+++ BackEnd/Materia/Aplicacion/Services/Consultar/ConsultarMateriaService.cs
@@ -0,0 +1,36 @@
+﻿using BackEnd.Base;
+using BackEnd.Nota.Aplicacion.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BackEnd.Nota.Dominio;
+
+namespace BackEnd.Materia.Aplicacion.Services.Consultar
+{
+    public class ConsultarMateriaService
+    {
+        readonly IUnitOfWork _unitOfWork;
+
+
+        public ConsultarMateriaService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+
+        public List<Nota.Dominio.Entidades.Nota> GetAll()
+        {
+            var res = _unitOfWork.NotaServiceRepository.FindBy();
+            _unitOfWork.Dispose();
+            return res.ToList();
+        }
+
+        public Nota.Dominio.Entidades.Nota GetId(int id)
+        {
+            var ConsultarID = _unitOfWork.NotaServiceRepository.Find(id);
+            _unitOfWork.Dispose();
+            return ConsultarID;
+        }
+    }
+}
