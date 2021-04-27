e461441324309cd4a0d1ec6666992aaff0d1cf16
CarlosDaza <2carlosdaza@gmail.com>
2021-04-27 12:22:58 -0500

    Registrar Curso Corregido v2

--- SiColegioMiHogar/ClientApp/src/app/curso/form-curso/form-curso.component.ts
+++ SiColegioMiHogar/ClientApp/src/app/curso/form-curso/form-curso.component.ts
@@ -20,7 +20,7 @@ export class FormCursoComponent implements OnInit {
     private docenteService: DocenteService, private cursoService: CursoService, private alertService: AlertService) { }
   formGroup = this.fb.group({
     nombre: ['', [Validators.required]],
-    maxEstudiantes: [1, [Validators.required]],
+    maxEstudiantes: [10, [Validators.required]],
     idDirectorDocente: [4, [Validators.required]],
   });
   
@@ -33,10 +33,13 @@ export class FormCursoComponent implements OnInit {
   }
   save() {
     let curso: ICurso = Object.assign({}, this.formGroup.value);
+    
     console.table(curso); //ver mensualidad por consola
+    curso.idDirectorDocente = +curso.idDirectorDocente;
+    console.table(curso);
     this.cursoService.createCurso(curso)
       .subscribe(curso => this.onSaveSuccess(),
-        error => this.alertService.error(error.error));
+        error => this.alertService.error(error));
   }
   onSaveSuccess() {
     this.router.navigate(["/cursos"]);
