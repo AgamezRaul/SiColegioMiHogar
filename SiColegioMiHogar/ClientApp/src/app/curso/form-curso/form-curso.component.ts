9c52d6afcc394784713ac449f0300091ac18bfd1
CarlosDaza <2carlosdaza@gmail.com>
2021-04-27 10:34:25 -0500

    consultar curso Corregido

--- SiColegioMiHogar/ClientApp/src/app/curso/form-curso/form-curso.component.ts
+++ SiColegioMiHogar/ClientApp/src/app/curso/form-curso/form-curso.component.ts
@@ -21,7 +21,7 @@ export class FormCursoComponent implements OnInit {
   formGroup = this.fb.group({
     nombre: ['', [Validators.required]],
     maxEst: [1, [Validators.required]],
-    idDocente: [1, [Validators.required]],
+    idDocente: [4, [Validators.required]],
   });
   
   ngOnInit(): void {
