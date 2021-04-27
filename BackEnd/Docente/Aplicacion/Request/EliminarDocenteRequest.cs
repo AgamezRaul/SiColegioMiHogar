4cd7e91f3122457972052743a8175022dc80ba89
CarlosDaza <2carlosdaza@gmail.com>
2021-04-26 20:03:16 -0500

    Backend Eliminar Docente

---
+++ BackEnd/Docente/Aplicacion/Request/EliminarDocenteRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd.Docente.Aplicacion.Request
+{
+    public class EliminarDocenteRequest
+    {
+        public int IdDocente { get; set; }
+    }
+    public class EliminarDocenteResponse
+    {
+        public string Message { get; set; }
+        public bool isOk()
+        {
+            return this.Message.Equals("Docente Eliminado Exitosamente");
+        }
+    }
+}
