304d80c519a1456badaed31e30e77b52fafe9b75
micheelRojas <56304710+micheelRojas@users.noreply.github.com>
2021-04-28 23:01:12 -0500

    Esquema de carpeta de UsuarioDocente

---
+++ BackEnd/UsarioDocente/Dominio/UsuarioDocente.cs
@@ -0,0 +1,38 @@
+﻿using BackEnd.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd.UsarioDocente.Dominio
+{
+   public class UsuarioDocente : Entity<int>
+    {
+       
+
+        public int IdDocente { get; set; }
+        public string Correo { get; set; }
+        public string Password { get; set; }
+        
+        public string TipoUsuario { get; set; }
+        public UsuarioDocente() { }
+        public UsuarioDocente(int idDocente, string correo, string password, string tipoUsuario)
+        {
+            IdDocente = idDocente;
+            Correo = correo;
+            Password = password;
+            TipoUsuario = tipoUsuario;
+        }
+        public IReadOnlyList<string> CanCrear(UsuarioDocente usarioDocente)
+        {
+            var errors = new List<string>();
+
+            if (usarioDocente.IdDocente == 0)
+                errors.Add("Campo identifacion del docente vacio");
+            if (string.IsNullOrEmpty(usarioDocente.Correo))
+                errors.Add("Campo correo vacio");
+            if (string.IsNullOrEmpty(usarioDocente.Password))
+                errors.Add("Campo Contraseña vacio");
+            return errors;
+        }
+    }
+}
---
+++ BackEnd/UsarioDocente/Infra/IUsuarioDocenteServiceRepository.cs
@@ -0,0 +1,12 @@
+﻿using BackEnd.Base;
+using BackEnd.UsarioDocente.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd.UsarioDocente.Infra
+{
+    public interface IUsuarioDocenteServiceRepository : IGenericRepository<UsuarioDocente>
+    {
+    }
+}
