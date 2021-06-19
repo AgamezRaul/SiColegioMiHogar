ffd01200c1ccdd58b708066c6ed929cca3935774
micheelRojas <56304710+micheelRojas@users.noreply.github.com>
2021-06-18 19:08:44 -0500

    Consulta Para terminar Raul PDF

---
+++ BackEnd/Boletin/Aplicacion/Request/BoletinPDFRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace BackEnd.Boletin.Aplicacion.Request
+{
+    public class BoletinPDFRequest
+    {
+        public string IdEstudiante { get; set; }
+        public string NombreEstudiante { get; set; }
+        public string CursoEstudiante { get; set; }
+        public string Jornada { get; set; }
+        public int Periodo { get; set; }
+        public int Puesto { get; set; }
+        public int Anio { get; set; }
+        public string Observaciones { get; set; }
+        public string DocenteCurso { get; set; }
+        List<ListaMaterias> listaMaterias { get; set; }
+    }
+
+    public class ListaMaterias
+    {
+        public string NombreMateria { get; set; }
+        public int Fallas { get; set; }
+        public double NotaPeriodoUno { get; set; }
+        public double NotaPeriodoDos { get; set; }
+        public double NotaPeriodoTres { get; set; }
+        public double NotaPeriodoCuarto { get; set; }
+        public double NotaPromedio { get; set; }
+        public string Desempenio { get; set; }
+    }
+}
--- BackEnd/Boletin/Aplicacion/Services/GenerarBoletin/GenerarBoletinService.cs
+++ BackEnd/Boletin/Aplicacion/Services/GenerarBoletin/GenerarBoletinService.cs
@@ -13,24 +13,33 @@ using Document = iTextSharp.text.Document;
 using Rectangle = iTextSharp.text.Rectangle;
 using BackEnd.Base;
 using System.Runtime.InteropServices;
+using BackEnd.Estudiante.Aplicacion.Request;
+using BackEnd.Nota.Aplicacion.Request;
+using BackEnd.Periodo.Aplicacion.Request;
+using BackEnd.Materia.Aplicacion.Request;
+using BackEnd.Curso.Aplicacion.Request;
 
 namespace BackEnd.Boletin.Aplicacion.Services.GenerarBoletin
 {
     public class GenerarBoletinService
     {
         readonly IUnitOfWork _unitOfWork;
+        readonly MiHogarContext _context;
+        readonly BoletinPDFRequest _boletinPDFRequest;
         private static Guid FolderDownloads = new Guid("374DE290-123F-4565-9164-39C4925E467B");
         [DllImport("shell32.dll", CharSet = CharSet.Auto)]
         private static extern int SHGetKnownFolderPath(ref Guid id, int flags, IntPtr token, out IntPtr path);
-        public GenerarBoletinService(IUnitOfWork unitOfWork)
+        public GenerarBoletinService(MiHogarContext context)
         {
-            _unitOfWork = unitOfWork;
+            _context = context;
+            _unitOfWork = new UnitOfWork(_context);
+            _boletinPDFRequest = new BoletinPDFRequest();
         }
         string ruta = GetDownloadsPath()+ @"\BoletinPDF.pdf";
         CrearBoletinRequest Boletin;
-        public void GuardarPdfBoletin(CrearBoletinRequest boletin)
+        public void GuardarPdfBoletin(BoletinPDFRequest boletinPDFRequest)
         {
-            Boletin = boletin;
+           
             FileStream fs = new FileStream(ruta, FileMode.Create);
             Document document = new Document(iTextSharp.text.PageSize.LEGAL, 40, 40, 40, 40);
             PdfWriter pw = PdfWriter.GetInstance(document, fs);
@@ -80,25 +89,68 @@ namespace BackEnd.Boletin.Aplicacion.Services.GenerarBoletin
             document.Add(imagen);*/
             //lo que dimos en clase 
 
-            document.Add(new Paragraph($"       Periodo: {boletin.IdPeriodo}  \t Fecha de generacion: {boletin.FechaGeneracion}"));
+           /* document.Add(new Paragraph($"      Nombre del Periodo: {periodo.NombrePeriodo} \t  #: {periodo.NumeroPeriodo} \t Fecha de generacion: {boletin.FechaGeneracion}"));
+            document.Add(new Paragraph("\n"));
+            document.Add(new Paragraph($"      Curso: {curso.nombre} - {curso.Letra} \t  Estudiante: {estudiante.NomEstudiante} \t Identificacion: {estudiante.IdeEstudiante} \t Puesto: {puestos(curso)}" ));*/
             document.Add(new Paragraph("\n"));
             document.Add(new Paragraph("      ---------------------------------------------------------------------------------------------------------------"));
             document.Close();
         }
-        public string BoletinPDF(CrearBoletinRequest request)
+        public string puestos(CrearCursoRequest curso) {
+            //mirar como ordenar los puestos de cada integrante
+            return "3";
+        }
+        
+        public BoletinPDFRequest BoletinPDF(int idEstudiante, int idPeriodo)
         {
-            string resultado = string.Empty;
-            try
+            var materias = (from e in _context.Set<BackEnd.Estudiante.Dominio.Estudiante>()
+                            join ec in _context.Set<BackEnd.EstudianteCurso.Dominio.EstudianteCurso>()
+                            on e.Id equals ec.IdEstudiante
+                            join c in _context.Set<BackEnd.Curso.Dominio.Curso>()
+                            on ec.IdCurso equals c.Id
+                            join m in _context.Set<BackEnd.Materia.Dominio.Entidades.Materias>()
+                            on c.Id equals m.IdCurso
+                            where e.Id == idEstudiante
+                            select new
+                            {
+                                Id = m.Id,
+                                NombreMateria = m.NombreMateria,
+                            }).ToList();
+            foreach (var materia in materias)
             {
-                GuardarPdfBoletin(request);
-                resultado = "Boletin Generado";
+                
             }
-            catch (Exception er)
-            {
-                resultado = "Error al generar el boletin: " + er.Message;
-            }
-            Console.WriteLine(resultado);
-            return resultado;
+
+
+            var notaPeriodos = (from m in materias
+                                join np in _context.Set<BackEnd.NotaPeriodo.Dominio.NotaPeriodo>()
+                                on m.Id equals np.IdMateria
+                                select new
+                                {
+                                    Id = m.Id,
+                                    NombreMateria = m.NombreMateria,
+                                    Periodo = np.IdPeriodo,
+                                    NotaPeriodo = np.Nota
+                                }).ToList();
+
+
+            var estudiante = (from e in _context.Set<BackEnd.Estudiante.Dominio.Estudiante>()
+                              join ec in _context.Set<BackEnd.EstudianteCurso.Dominio.EstudianteCurso>()
+                              on e.Id equals ec.IdEstudiante
+                              join c in _context.Set<BackEnd.Curso.Dominio.Curso>()
+                              on ec.IdCurso equals c.Id
+                              join d in _context.Set<BackEnd.Docente.Dominio.Docente>()
+                              on c.IdDirectorDocente equals d.Id
+                              where e.Id == idEstudiante
+                              select new
+                              {
+                                  IdEstudiante = e.IdeEstudiante,
+                                  NombreEstudiante = e.NomEstudiante,
+                                  CursoEstudiante = c.Nombre + " " + c.Letra,
+                                  DocenteCurso = d.NombreCompleto,
+
+                              }).ToList();
+            
         }
         public static string GetDownloadsPath()
         {
@@ -116,6 +168,7 @@ namespace BackEnd.Boletin.Aplicacion.Services.GenerarBoletin
                 Marshal.FreeCoTaskMem(pathPtr);
             }
         }
+        
     }
 }
 
