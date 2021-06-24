5a3a0f3fa826766967d38a390403eb90d374a7f2
micheelRojas <56304710+micheelRojas@users.noreply.github.com>
2021-06-24 18:44:35 -0500

    Controlador basico de grado y abonos

---
+++ SiColegioMiHogar/Controllers/AbonoController.cs
@@ -0,0 +1,83 @@
+﻿using BackEnd;
+using BackEnd.Abono.Aplicacion.Request;
+using BackEnd.Abono.Aplicacion.Service.Actualizar;
+using BackEnd.Abono.Aplicacion.Service.Anular;
+using BackEnd.Abono.Aplicacion.Service.Crear;
+using BackEnd.Abono.Dominio;
+using BackEnd.Base;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiColegioMiHogar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AbonoController : ControllerBase
+    {
+        private readonly MiHogarContext _context;
+        private CrearAbonoService _service;
+        private ActualizarAbonoService _actualizarService;
+        private AnularAbonoService _anularService;
+        private UnitOfWork _unitOfWork;
+        public AbonoController(MiHogarContext context)
+        {
+            this._context = context;
+            _unitOfWork = new UnitOfWork(_context);
+        }
+        /*
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAbono([FromRoute] int id)
+        {
+            Abono abono = await _context.Abono.SingleOrDefaultAsync(t => t.Id == id);
+            if (abono == null)
+                return NotFound();
+            return Ok(abono);
+        }*/
+        [HttpPost]
+        public async Task<IActionResult> CreateAbono([FromBody] CrearAbonoRequest abono)
+        {
+            _service = new CrearAbonoService(_unitOfWork);
+            var rta = _service.Ejecutar(abono);
+            if (rta.isOk())
+            {
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetAbono", new { id = abono.id }, abono);
+            }
+            return BadRequest(rta.Message);
+        }
+
+
+        [HttpPut("PutAnular/{id}")]
+        public async Task<IActionResult> PutAnular([FromRoute] int id)
+        {
+            _anularService = new AnularAbonoService(_unitOfWork);
+            AnularAbonoRequest request = new AnularAbonoRequest();
+            request.id = id;
+            var rta = _anularService.Ejecutar(request);
+            if (rta.isOk())
+            {
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetAbono", new { id = request.id }, request);
+            }
+            return Ok(rta);
+        }
+
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAbono([FromRoute] int id, [FromBody] ActualizarAbonoRequest abono)
+        {
+            _actualizarService = new ActualizarAbonoService(_unitOfWork);
+            var rta = _actualizarService.Ejecutar(abono);
+            if (rta.isOk())
+            {
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetAbono", new { id = abono.id }, abono);
+            }
+            return BadRequest(rta.Message);
+        }
+    }
+}
---
+++ SiColegioMiHogar/Controllers/GradoController.cs
@@ -0,0 +1,80 @@
+﻿using BackEnd;
+using BackEnd.Base;
+using BackEnd.Grado.Aplicacion.Request;
+using BackEnd.Grado.Aplicacion.Service.Actualizar;
+using BackEnd.Grado.Aplicacion.Service.Crear;
+using BackEnd.Grado.Aplicacion.Service.Eliminar;
+using BackEnd.Grado.Dominio;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SiColegioMiHogar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GradoController : ControllerBase
+    {
+        private readonly MiHogarContext _context;
+        private CrearGradoService _service;
+        private ActualizarGradoService _actualizarService;
+        private EliminarGradoService _eliminarService;
+        private UnitOfWork _unitOfWork;
+        public GradoController(MiHogarContext context)
+        {
+            this._context = context;
+            _unitOfWork = new UnitOfWork(_context);
+        }
+
+       /* [HttpGet("{id}")]
+        public async Task<IActionResult> GetGrado([FromRoute] int id)
+        {
+            Grado grado = await _context.Grado.SingleOrDefaultAsync(t => t.Id == id);
+            if (grado == null)
+                return NotFound();
+            return Ok(grado);
+        }*/
+        [HttpPost]
+        public async Task<IActionResult> CreateGrado([FromBody] CrearGradoRequest grado)
+        {
+            _service = new CrearGradoService(_unitOfWork);
+            var rta = _service.Ejecutar(grado);
+            if (rta.isOk())
+            {
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetGrado", new { id = grado.id }, grado);
+            }
+            return BadRequest(rta.Message);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGrado([FromRoute] int id)
+        {
+            _eliminarService = new EliminarGradoService(_unitOfWork);
+            EliminarGradoRequest request = new EliminarGradoRequest();
+            request.id = id;
+            var rta = _eliminarService.Ejecutar(request);
+            if (rta.isOk())
+            {
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetGrado", new { id = request.id }, request);
+            }
+            return Ok(rta);
+        }
+
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGrado([FromRoute] int id, [FromBody] ActualizarGradoRequest grado)
+        {
+            _actualizarService = new ActualizarGradoService(_unitOfWork);
+            var rta = _actualizarService.Ejecutar(grado);
+            if (rta.isOk())
+            {
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetGrado", new { id = grado.id }, grado);
+            }
+            return BadRequest(rta.Message);
+        }
+    }
+}
