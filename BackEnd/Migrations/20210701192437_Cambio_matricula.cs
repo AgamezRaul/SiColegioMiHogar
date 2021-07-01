698172c74d07c61f911891cce3c7152d2ce0b28d
AgamezRaul <39956182+AgamezRaul@users.noreply.github.com>
2021-07-01 14:25:05 -0500

    Nueva migración

---
+++ BackEnd/Migrations/20210701192437_Cambio_matricula.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEnd.Migrations
+{
+    public partial class Cambio_matricula : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Periodo",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FechaFin", "FechaInicio" },
+                values: new object[] { new DateTime(2021, 7, 6, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2021, 6, 26, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Periodo",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "FechaFin", "FechaInicio" },
+                values: new object[] { new DateTime(2021, 7, 17, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2021, 7, 7, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Periodo",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "FechaFin", "FechaInicio" },
+                values: new object[] { new DateTime(2021, 7, 28, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2021, 7, 18, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Periodo",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "FechaFin", "FechaInicio" },
+                values: new object[] { new DateTime(2021, 8, 8, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2021, 7, 29, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Periodo",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FechaFin", "FechaInicio" },
+                values: new object[] { new DateTime(2021, 6, 29, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2021, 6, 19, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Periodo",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "FechaFin", "FechaInicio" },
+                values: new object[] { new DateTime(2021, 7, 10, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2021, 6, 30, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Periodo",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "FechaFin", "FechaInicio" },
+                values: new object[] { new DateTime(2021, 7, 21, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2021, 7, 11, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Periodo",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "FechaFin", "FechaInicio" },
+                values: new object[] { new DateTime(2021, 8, 1, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2021, 7, 22, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+    }
+}
